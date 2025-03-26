12f7365ec070d7bdc38780c2fa2a3c073bf1c851
laercio-siqueira <laerciobete@gmail.com>
2025-03-26 14:27:47 -0300

    Create master class tarefas.js
    teste do dia 22

---
+++ master class tarefas.js
@@ -0,0 +1,15 @@
+let lista_tarefas = [];
+
+
+function adicionar_tarefa(tarefa) {
+    if (tarefa && typeof tarefa === 'string') {
+        lista_tarefas.push(tarefa);
+        console.log(`Tarefa \"${tarefa}\" adicionada com sucesso!`);
+    } else {
+        console.log("Por favor, forneça uma tarefa válida em formato de string.");
+    }
+}
+
+
+adicionar_tarefa("Finalizar implementação da lista de tarefas");
+console.log("Lista atual de tarefas:", lista_tarefas);
